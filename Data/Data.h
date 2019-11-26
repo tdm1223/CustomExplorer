c2166f64ea6ec7083b6e31c0902ecdf59ed53078
tdm1223 <tdm1223@naver.com>
2019-11-26 20:31:23 +0900

    [UPDATE] DATA 코드 개선
    프로토콜 종류 수정

--- Data/Data.h
+++ Data/Data.h
@@ -4,8 +4,8 @@
 enum Protocol
 {
     kConnect = 100,
-    kRefreshTreeCtrl = 200,
-    kRefreshListCtrl = 300,
+    kUpdateTreeCtrl = 200,
+    kUpdateListCtrl = 300,
     kRequestData = 400
 };
 enum FileType
@@ -18,21 +18,21 @@ enum FileType
 class Data
 {
 public:
+    Data();
+    ~Data();
+
+    void Serialize(Data & file, char * buffer);
+    void DeSerialize(Data & file, char * buffer);
+
     Protocol protocol;
+    FileType fileType;
+    FileType childType[500];
     char fileName[MAX_PATH];
     char filePath[1024];
     int fileSize;
-    FileType fileType;
     int childLength;
     char child[500][MAX_PATH];
     char childAccessTime[500][100];
     int childSize[500];
-    FileType childType[500];
-
-    Data();
-    ~Data();
-    void Serialize(Data & file, char * buffer);
-    void DeSerialize(Data & file, char * buffer);
-
 };
 
